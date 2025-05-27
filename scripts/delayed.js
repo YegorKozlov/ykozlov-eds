3dcc62da9de588e9037f477d9797260a309ee11b
Egor Kozlov <egor.kozlov@fleetcor.com>
2025-05-27 18:13:34 +0200

    + analytics

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,1 +1,21 @@
-// add delayed functionality here
+import {
+  loadScript,
+} from './lib-franklin.js';
+
+export const isProd = () => {
+  const { host } = window.location;
+  if (host.includes('ykozlov.com')) {
+    return true;
+  }
+  return false;
+};
+
+const loadAdobeDTM = async () => {
+  if (isProd()) {
+    await loadScript('<script src="https://assets.adobedtm.com/ed8972503195/3ce0a0cfa076/launch-f235e81976f5.min.js');
+  } else {
+    await loadScript('https://assets.adobedtm.com/ed8972503195/3ce0a0cfa076/launch-1e4514e79705-development.min.js');
+  }
+};
+
+await loadAdobeDTM();
